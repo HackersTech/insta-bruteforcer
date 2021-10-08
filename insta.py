a1a8139124e0eae3a790e47d8733bd2c4d27b589
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-10-08 12:08:24 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -4,7 +4,7 @@ print('instagram id - \033[1;32m hackers__tech\033[0m')
 
 import os
 os.system('bash banner.sh')
-e=input("enter email:pass txt file ")
+e=input("enter email:pass txt file -:")
 with open(e,'r') as file:
     i =file.readlines()
     for k in i:
