ade57b25b6205fb12402360bf1b6ba55e72d0ba6
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-10-08 09:40:02 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+cd ..
+rm -rf insta-bruteforcer
+echo "updating ✓ ....." |lolcat -p 0.3
+git clone https://github.com/HackersTech/insta-bruteforcer/
+echo "updated ||||||||||||"|lolcat 
+cd insta-bruteforcer
+python3 insta.py
